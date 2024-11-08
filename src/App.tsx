542903a53f96b259e0fb073f6166d3e6b9b6b296
steveen ordoñez <zh311505@gmail.com>
2024-11-08 11:42:41 -0500

    fe: config slint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const hola = 'hola';
+  const hola = 'holaaaaa';
   return (
     <div className="App">
       <header className="App-header">
