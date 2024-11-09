784ec547715db397a32be4e816e7371d63e6ff31
HECTOR STEVEEN ORDOÑEZ CHAMBA <hsc.ordonez@yavirac.edu.ec>
2024-11-08 23:00:30 -0500

    fix: solved error and folder auth

--- src/router/MainRoutes.tsx
+++ src/router/MainRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginScreen from '../pages/modules/auth/login/LoginScreen';
+import LoginScreen from '../pages/auth/login/LoginScreen';
 
 const MainRoutes: React.FC = () => {
   return (
